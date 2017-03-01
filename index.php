c1009e29ee042200b1d64dc543e95e73fcfecac4
ernixl <ernixl@yahoo.com>
2017-03-01 17:48:15 +0000

    first commit

--- index.php
+++ index.php
@@ -11,9 +11,10 @@ $saus = "sausage rolls are available";
 $specs = "specs are available";
 
 for ($d = 1; $d <=30; $d ++)
-    if ($d % 2 == 0) {
-        print "<p>$d.It's even even</p>";
+    if ($d % 4 == 0) {
+        print "<p>$d.It's a mug</p>";
     }elseif($d % 2 != 0) {print "<p>$d.it is odd</p>";}
+    else print "thats all left";
 
 ?>
 
