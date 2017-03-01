8e2bf00df673e04d1f35abe6435d2a5d5a446626
ernixl <ernixl@yahoo.com>
2017-03-01 18:05:19 +0000

    first commit

--- index.php
+++ index.php
@@ -13,11 +13,11 @@ $specs = "specs are available";
 for ($d = 1; $d <= 30; $d++)
     if ($d % 4 == 0) {
         print "<p>on the " . $d . " of this month sausage rolls are available</p>";
-    } elseif ($d % 1 == 1) {
+    } elseif ($d - 1 == 0) {
         print "<p>on the " . $d . " of this month mugs are available</p>";
     } elseif ($d % 3 != 0) {
         print "<p>on the " . $d . " of this month no products are available</p>";
-    } elseif ($d % 2 != 0) print "on the " . $d . " of this month specsavers are available";
+    } elseif ($d % 2 != 0) print "on the " . $d . " of this month specs are available";
 
 ?>
 
