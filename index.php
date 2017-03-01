262be678c442aaa111f2e09c90f86b985d08770b
ernixl <ernixl@yahoo.com>
2017-03-01 19:23:33 +0000

    first commit

--- index.php
+++ index.php
@@ -6,12 +6,13 @@
 
 <?php
 
-$random = rand(1,30);
+$random = rand(1, 30);
 
 for ($d = 1; $d <= 30; $d++)
-    if ($d == $random) {
+    if ($d = $random) {
         print "<p>on the " . $d . " of this month no products are available</p>";
-}elseif ( $d==$random){print "<p>on the " . $d . " of this month no products are available</p>";
+    } elseif ($d = $random) {
+        print "<p>on the " . $d . " of this month no products are available</p>";
     }
 
 
