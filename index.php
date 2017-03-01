b828525cc8dfb6bab48b5acc91f295f2858be6e5
ernixl <ernixl@yahoo.com>
2017-03-01 20:27:33 +0000

    first commit

--- index.php
+++ index.php
@@ -10,7 +10,10 @@ $spec=1;
 $sausages=2;
 $random = rand(0,2);
 
-echo "$random"
+if ($random==0){print "mugs"; }
+elseif ($random=1){print "specs";}
+elseif ($random=2){print "sausages";}
+
 
 
 ?>
