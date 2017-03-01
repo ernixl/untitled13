b58af1eebe87bcd60d89de460d67bd0964c07ca9
ernixl <ernixl@yahoo.com>
2017-03-01 19:19:41 +0000

    first commit

--- index.php
+++ index.php
@@ -6,18 +6,13 @@
 
 <?php
 
-$random = rand(5,7,11,13,17,19,23);
+$random = rand(1,30);
 
 for ($d = 1; $d <= 30; $d++)
-    if ($d == 1) {
+    if ($d == $random) {
         print "<p>on the " . $d . " of this month no products are available</p>";
-    } elseif ($d % 4 == 0) {
-        print "<p>on the " . $d . " of this month sausage rolls are available</p>";
-    } elseif ($d % 3 == 0) {
-        print "<p>on the " . $d . " of this month mugs are available</p>";
-    } elseif ($d % 2 == 0) {
-        print "on the " . $d . " of this month specs are available";
-    }
+}
+
 
 ?>
 
