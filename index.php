2ac0e7a065ffab7d532cdb034b19d41d2a8dc9d0
ernixl <ernixl@yahoo.com>
2017-03-01 17:53:06 +0000

    first commit

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ for ($d = 1; $d <=30; $d ++)
     if ($d % 4 == 0) {
         print "<p>$d.It's a mug</p>";
     }elseif($d % 2 != 0) {print "<p>$d.it is odd</p>";}
-    else print "on the".$d."thats all left";
+    else print "on the ".$d." of this monthhats specs are available";
 
 ?>
 
