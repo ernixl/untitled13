78ef51478892c02ff4b53332402c6ab4ed7a96fd
ernixl <ernixl@yahoo.com>
2017-03-01 15:50:54 +0000

    first commit

--- index.php
+++ index.php
@@ -14,6 +14,10 @@ for ($i = 1; $i <= 30; $i += 2) {
     echo "On the " . "$i. mugs are available!<br />";
 }
 
+for ($i = 4; $i <= 30; $i *= 4) {
+    echo "On the " . "$i. sausage rolls are available!<br />";
+}
+
 
 ?>
 
