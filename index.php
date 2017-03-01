39dab3b23cf6a819f2879d6c5ef8f91ea679146e
ernixl <ernixl@yahoo.com>
2017-03-01 12:06:09 +0000

    first commit

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
 <?php
 
 $provisionedactivity = array("specs ", "mugs ", "sausage-rolls");
-$provisionedactivity [2] = "hugs";
-unset($provisionedactivity[3]);
+$provisionedactivity [1] = "hugs";
+unset($provisionedactivity[2]);
 
 foreach ($provisionedactivity as $x) {
     print "$x";
