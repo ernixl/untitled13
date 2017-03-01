8e2cc71d46e3516f9d995573e85f10110a811cc2
ernixl <ernixl@yahoo.com>
2017-03-01 06:45:00 +0000

    first commit

--- index.php
+++ index.php
@@ -11,9 +11,13 @@ if ($age >= "16" or $age <= "18") {
     print "specs, ";
 }
 
-if ($age >= "18" or $age <= "21"){print "mugs, ";}
+if ($age >= "18" or $age <= "21") {
+    print "mugs, ";
+}
 
-if ($age>"21"){print "sausage rolls.";}
+if ($age > "21") {
+    print "sausage rolls.";
+}
 
 
 ?>
