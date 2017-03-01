5eb559498b15edbd4b733dbdc7a1147012fb1e1b
ernixl <ernixl@yahoo.com>
2017-03-01 17:58:38 +0000

    first commit

--- index.php
+++ index.php
@@ -10,11 +10,12 @@ $mugs = "mugs are available";
 $saus = "sausage rolls are available";
 $specs = "specs are available";
 
-for ($d = 1; $d <=30; $d ++)
+for ($d = 1; $d <= 30; $d++)
     if ($d % 4 == 0) {
-        print "<p>on the ".$d. " of this month sausage rolls are available</p>";
-    }elseif($d % 2 != 0) {print "<p>$d.it is odd</p>";}
-    else print "on the ".$d." of this month specs are available";
+        print "<p>on the " . $d . " of this month sausage rolls are available</p>";
+    } elseif ($d % 3 != 0) {
+        print "<p>on the " . $d . " of this month mugs are available</p>";
+    } else print "on the " . $d . " of this month specs are available";
 
 ?>
 
