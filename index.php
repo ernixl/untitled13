76199b681d89176dbcb9ef3303d0309bbe40fe95
ernixl <ernixl@yahoo.com>
2017-03-01 18:21:51 +0000

    first commit

--- index.php
+++ index.php
@@ -6,18 +6,15 @@
 
 <?php
 
-$mugs = "mugs are available";
-$saus = "sausage rolls are available";
-$specs = "specs are available";
 
 for ($d = 1; $d <= 30; $d++)
-    if ($d % 4 == 0) {
+    if ($d == 1) {
+        print "<p>on the " . $d . " of this month sno products are available</p>";
+    } elseif ($d % 4 == 0) {
         print "<p>on the " . $d . " of this month sausage rolls are available</p>";
-    } elseif ($d - 1 == 0) {
+    } elseif ($d % 3 == 0) {
         print "<p>on the " . $d . " of this month mugs are available</p>";
-    } elseif ($d % 3 != 0) {
-        print "<p>on the " . $d . " of this month no products are available</p>";
-    } elseif ($d % 2 != 0) print "on the " . $d . " of this month specs are available";
+    } elseif ($d % 2 == 0) print "on the " . $d . " of this month specs are available";
 
 ?>
 
