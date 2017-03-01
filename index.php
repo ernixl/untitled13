33d4f366f9db42a3e73d7f5927fdbe0f418e9c14
ernixl <ernixl@yahoo.com>
2017-03-01 16:59:25 +0000

    first commit

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $specs = "specs are available";
 
 for ($d = 1; $d <=30; $d ++)
     if ($d % 2 == 0) {
-        print "$d.It's even";
+        print "$d.It's even even";
     }elseif($d % 2 != 0) {print "$d.it is odd";}
 
 ?>
